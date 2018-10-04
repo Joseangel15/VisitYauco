9b36878c5bc4664e4d17bd5015c5492480b90d85
Jose <joseangelortiz15@gmail.com>
2018-10-04 14:37:19 -0600

    fixed static

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ const nodemailer = require('nodemailer');
 const axios = require('axios');
 const app = express();
 
-app.use(express.static(`${__dirname}/build`))
+app.use(express.static(`${__dirname}/../../build`))
 
 const {
 
